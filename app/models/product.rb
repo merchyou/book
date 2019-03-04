fb76bc60759660f26117eec882d5476dbefeec3e
Michał Marchel <m.marchel@MacBook-Air-Marchel.local>
2019-03-04 12:15:05 +0100

    c1 creating the catalog listing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,11 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: {minimum: 10, message: "poczebuje wyncyj znakow!"}
   validates :image_url, allow_blank: true, format: {
     with:     %r{\.(gif|jpg|png)\Z}i,
     message:  'must be a URL for GIF, JPG, or PNG image.'
   }
   validates :price, numericality: {greater_than_or_equal_to: 0.01 }
+
 
 end
